ed4e2c6eddb7fe0d70b044ea98178d8f179c8191
musalem <111184820+swe-musalem@users.noreply.github.com>
2022-10-26 16:55:29 +0300

    handling syntax and name  errors

--- cmdapp/pycmd/pycmdapp/views.py
+++ cmdapp/pycmd/pycmdapp/views.py
@@ -1,23 +1,35 @@
-from multiprocessing import context
+from re import I
 from django.shortcuts import render
 import time
-# these libraryes is used to be able to catch the output out of exec
+# these libraryes is used to be able to catch the output out of exec function
 import sys
 from io import StringIO
 
 
 inputlst = []
+errorlst= []
+
 def cmd(request):
-    
     if request.method == 'POST':
         request.session['input'] =  request.POST.get('input')
         old_stdout = sys.stdout
         redirected_output = sys.stdout = StringIO()
-        exec(request.POST.get('input'))
-        sys.stdout = old_stdout
+        try:
+            exec(request.POST.get('input'))
+        except NameError as err:
+            
+            request.session['err'] = str(err)
+            errorlst.append(request.session['err'] )
+
+        except SyntaxError as err:
+        
+            request.session['err'] = str(err)
+            errorlst.append(request.session['err'] )
+            
         inputlst.append(redirected_output.getvalue())
         
     context = {
-            'inputlst': inputlst
+            'inputlst': inputlst,
+            'errorlst':errorlst,
         }
     return render(request,'cmd.html',context)
